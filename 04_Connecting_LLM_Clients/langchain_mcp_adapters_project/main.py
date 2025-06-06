23f2c719c9006485a74a3124177c2e2abc1385ee
felipaonick <felipaosfdk@yahoo.com>
2025-06-06 16:55:24 +0200

    Enhance main.py to include HumanMessage for agent interaction and update async main function to initialize session and invoke agent with a sample query.

--- 04_Connecting_LLM_Clients/langchain_mcp_adapters_project/main.py
+++ 04_Connecting_LLM_Clients/langchain_mcp_adapters_project/main.py
@@ -9,7 +9,7 @@ from mcp.client.stdio import stdio_client
 from langchain_openai import ChatOpenAI
 from langchain_mcp_adapters.tools import load_mcp_tools
 from langgraph.prebuilt import create_react_agent
-
+from langchain_core.messages import HumanMessage
 
 llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
 
@@ -19,10 +19,20 @@ stdio_server_params = StdioServerParameters(
 )
 
 
-
-
 async def main():
-    print("Hello from langchain-mcp-adapters-project!")
+    async with stdio_client(stdio_server_params) as (read, write):
+        async with ClientSession(read_stream=read, write_stream=write) as session:
+            await session.initialize()
+            print("Sessione inizializzata")
+            tools = await load_mcp_tools(session)
+            print(tools)
+
+            agent = create_react_agent(model=llm, tools=tools)
+
+            response = await agent.ainvoke({
+                "messages": [HumanMessage(content="What is 2 plus 2?")]
+            })
+            print(response['messages'][-1].content)
 
 
 if __name__ == "__main__":
